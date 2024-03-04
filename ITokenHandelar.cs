74027fc40af9e5bfc14569388086411bb4442262
M N Tushar <45463161+mntushar@users.noreply.github.com>
2024-03-04 12:37:03 +0600

    Add files via upload
    update user information

--- ITokenHandelar.cs
+++ ITokenHandelar.cs
@@ -5,8 +5,10 @@ namespace DNE.CS.Inventory.Library.Interface
     public interface ITokenHandelar
     {
         Task<ClaimsPrincipal> UserInformation();
+        ClaimsPrincipal UserInformation(string accessToken);
         Task<string?> GetAccessTokenAsync();
         Task<string?> RefreshToken();
-
+        Task SetTokenAsync(string accessToken, string refreshToken);
+        Task ClearTokenAsync();
     }
 }
--- TokenHandelar.cs
+++ TokenHandelar.cs
@@ -42,10 +42,30 @@ public class TokenHandelar : ITokenHandelar
         return user;
     }
 
+    public ClaimsPrincipal UserInformation(string accessToken)
+    {
+        var identity = new ClaimsIdentity();
+
+        if (accessToken != null)
+        {
+            var claims = ParseClaimsFromJwt(accessToken);
+            if (claims.Any())
+            {
+                identity = new ClaimsIdentity(claims, "jwt");
+            }
+        }
+
+        var user = new ClaimsPrincipal(identity);
+
+        return user;
+    }
+
     public async Task<string?> GetAccessTokenAsync()
     {
         try
         {
+            if (AppInformation.AccessTokenName == null) return null;
+
             string? accessToken = await _localStorage.GetItemAsync<string>(
                 AppInformation.AccessTokenName);
 
@@ -89,7 +109,7 @@ public class TokenHandelar : ITokenHandelar
             TimeZoneInfo localTimeZone = TimeZoneInfo.Local;
             DateTime TokenlocalDateTime = TimeZoneInfo.ConvertTimeFromUtc(tokenDate.DateTime, localTimeZone);
 
-            return TokenlocalDateTime >= DateTime.Now;
+            return (DateTime.Now - TokenlocalDateTime).TotalSeconds >= 30;
         }
         catch (Exception ex)
         {
@@ -112,7 +132,8 @@ public class TokenHandelar : ITokenHandelar
         if (refreshAccessToken.IsSuccess)
         {
             refreshToken = refreshAccessToken.Data;
-
+            if (AppInformation.AccessTokenName == null
+                || refreshToken == null) return null;
             await _localStorage.SetItemAsStringAsync(AppInformation.AccessTokenName,
                 refreshToken);
 
@@ -124,14 +145,30 @@ public class TokenHandelar : ITokenHandelar
         return null;
     }
 
+    public async Task SetTokenAsync(string accessToken, string refreshToken)
+    {
+        if (AppInformation.AccessTokenName == null
+            || AppInformation.RefreshTokenName == null) return;
+
+        await ClearTokenAsync();
+
+        await _localStorage.SetItemAsync(AppInformation.AccessTokenName,
+                accessToken);
+        await _localStorage.SetItemAsync(AppInformation.RefreshTokenName,
+                refreshToken);
+    }
+
     public async Task ClearTokenAsync()
     {
+        if (AppInformation.AccessTokenName == null
+            || AppInformation.RefreshTokenName == null) return;
         await _localStorage.RemoveItemAsync(AppInformation.AccessTokenName);
         await _localStorage.RemoveItemAsync(AppInformation.RefreshTokenName);
     }
 
     public async Task<string?> RefreshToken()
     {
+        if (AppInformation.RefreshTokenName == null) return null;
         return await _localStorage.GetItemAsync<string>(
             AppInformation.RefreshTokenName);
     }
